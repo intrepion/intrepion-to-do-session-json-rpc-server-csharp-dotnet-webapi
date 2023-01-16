297d4b301e85fc16338e65f3463ef6e4059033ac
Oliver Forral <intrepion@gmail.com>
2023-01-16 11:15:40 -0700

    red - testing the health check controller for 200 status

---
+++ ToDoApp.Tests/WebApi/JsonRpc/TestJsonRpcController.cs
@@ -0,0 +1,11 @@
+namespace ToDoApp.Tests.WebApi.JsonRpc;
+
+public class TestJsonRpcController
+{
+    [Fact]
+    public void Get_Returns200()
+    {
+        // Arrange
+        var controller = new JsonRpcController();
+    }
+}
