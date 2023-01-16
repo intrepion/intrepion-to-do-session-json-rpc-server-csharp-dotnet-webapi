a30a8a8cc61e0035c4a4dc8ad297376c4f6472b6
Oliver Forral <intrepion@gmail.com>
2023-01-16 11:15:15 -0700

    red - testing the health check endpoint

---
+++ ToDoApp.Tests/Endpoints/TestHealthCheckEndpoint.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ToDoApp.Tests.WebApi.HealthCheck;
+
+public class TestHealthCheckEndpoint : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public TestHealthCheckEndpoint(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/HealthCheck");
+        var actual = response.Content.Headers.ContentType?.ToString();
+
+        // // Assert
+        Assert.NotNull(response);
+        response.EnsureSuccessStatusCode();
+    }
+}
